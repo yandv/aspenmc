4cc8ceb343c06e722663cb5becb4990afe16fc8d
yandv <allanmxr@gmail.com>
2023-07-06 01:25:19 -0300

    Adicionado formatação do chat

--- aspenmc-bukkit/src/main/java/br/com/aspenmc/bukkit/listener/ChatListener.java
+++ aspenmc-bukkit/src/main/java/br/com/aspenmc/bukkit/listener/ChatListener.java
@@ -44,7 +44,7 @@ public class ChatListener implements Listener {
         if (member == null) return;
 
         MessageBuilder messageBuilder = new MessageBuilder(
-                member.getTag().map(Tag::getRealPrefix).orElse("§7") + player.getName() + " §7" + event.getMessage());
+                member.getTag().map(Tag::getRealPrefix).orElse("§f") + player.getName() + " §8» §f" + event.getMessage());
 
         event.getRecipients().forEach(recipient -> recipient.spigot().sendMessage(messageBuilder.create()));
     }
